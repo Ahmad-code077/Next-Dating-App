74ffe851a98e5e85ccb147b0c22103ee6ff56f06
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2025-04-20 01:52:28 +0500

    Remove the old code that is not using promise.all

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,38 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { membersData } from './membersData';
 import { hash } from 'bcryptjs';
 const prisma = new PrismaClient();
-// async function seedMembers() {
-//   return membersData.map(async (singlemember) =>
-//     prisma.user.create({
-//       data: {
-//         email: singlemember.email,
 
-//         emailVerified: new Date(),
-//         name: singlemember.name,
-//         passwordHash: await hash('password', 10),
-//         image: singlemember.image,
-//         member: {
-//           create: {
-//             dateOfBirth: new Date(singlemember.dateOfBirth),
-//             gender: singlemember.gender,
-//             name: singlemember.name,
-//             created: new Date(singlemember.created),
-//             updated: new Date(singlemember.lastActive),
-//             description: singlemember.description,
-//             city: singlemember.city,
-//             country: singlemember.country,
-//             image: singlemember.image,
-//             photos: {
-//               create: {
-//                 url: singlemember.image,
-//               },
-//             },
-//           },
-//         },
-//       },
-//     })
-//   );
-// }
 async function seedMembers() {
   await Promise.all(
     membersData.map(async (singlemember) =>
